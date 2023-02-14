068d057dca9c702939f95335fa3053b07e7888ed
Yeri Han <hanyeri0223@gmail.com>
2023-02-14 19:40:49 +0800

    Implement my code

---
+++ biogps/y_preprocess.R
@@ -0,0 +1,15 @@
+#install packages
+install.packages("readr")
+
+#library installation
+library(readr)
+
+#load expression data
+x <- read.tsv("tcga-pancan-expr.tsv")
+
+# map probe to genes
+
+# remove NA genes
+
+
+# remove duplicated genes
