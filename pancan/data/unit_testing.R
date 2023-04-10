01af8ab910960f2e779651412e221e9a27a39fd4
Yeri Han <hanyeri0223@gmail.com>
2023-04-10 17:23:11 +0800

    Uploaded unit testing.R

---
+++ pancan/data/unit_testing.R
@@ -0,0 +1,19 @@
+library(io)
+library(ggplot2)
+
+testdf <- qread("unit testing.csv");
+
+testidx <- c(1,1,1,2,2,2,3,3,3,4,4,4)
+
+testmat <- testdf[,"expression"]
+
+testwithin_means = tapply(testmat, testidx, mean, na.rm=TRUE)
+testwithin_sd <- sqrt(
+    sum((testmat - testwithin_means[testidx])^2, na.rm=TRUE) / 
+      (length(testmat) - 1))
+  
+  
+# calculate standard deviation between the groups
+testoverall_mean = mean(testwithin_means)
+  
+testbetween_sd = sqrt(sum((testwithin_means-testoverall_mean)^2)/(length(testwithin_means)-1))
