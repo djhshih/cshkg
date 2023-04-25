27c234dde03f496110cb6f08f4c656fcb98527c9
Yeri Han <hanyeri0223@gmail.com>
2023-04-25 23:54:56 +0800

    Update sd_function.R

--- pancan/data/sd_function.R
+++ pancan/data/sd_function.R
@@ -3,16 +3,17 @@
 
 #Calculate within-group standard deviation
 get_within.sd <- function(v, group){
-  within_mean <- tapply(v, group, mean, na.rm = TRUE)
-  within_sd = sqrt(sum((v-within_mean[group])^2, na.rm = TRUE) / (length(v)-1))
+  group_mean <- tapply(v, group, mean, na.rm = TRUE)
+  within_sd = sqrt(sum((v-group_mean[group])^2, na.rm = TRUE) / (length(v)-1))
   return(within_sd)
 }
 
 #Calculate between-group standard deviation
 get_btwn.sd <- function(v, group){
-  within_mean <- tapply(v, group, mean, na.rm = TRUE)
-  overall_mean = mean(within_mean)
-  between_sd = sqrt(sum((within_mean-overall_mean)^2) / (length(within_mean)-1))
+  group_mean <- tapply(v, group, mean, na.rm = TRUE)
+  overall_mean <- mean(group_mean, na.rm=TRUE)
+  #print(sum((group_mean-overall_mean)^2))
+  between_sd <- sqrt(sum((group_mean-overall_mean)^2, na.rm=TRUE) / (length(group_mean)-1))
   return(between_sd)
 }
 
