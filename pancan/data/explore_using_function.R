b419aa316a41ae1723fc29995374adce1dc84595
Yeri Han <hanyeri0223@gmail.com>
2023-04-25 23:53:47 +0800

    Remove NaN from d.sd

--- pancan/data/explore_using_function.R
+++ pancan/data/explore_using_function.R
@@ -12,10 +12,7 @@ type.info <- qread("../annot/sample-type-code_tcga.tsv")
 
 
 x <- qread("expr_pancan.rds")
-mat0 <- x$data;
-
-# NB beware of copy-on-write
-mat <- mat0;
+mat <- x$data;
 
 # Grouping the tissues
 idx <- match(substring(colnames(mat),1,16), proj.info$sample)
@@ -38,11 +35,17 @@ rare.groups <- names(which(table(pheno$group) < 10));
 
 levels(pheno$group)[levels(pheno$group) %in% rare.groups] <- NA;
 
+idx.remove <- is.na(pheno$group);
+table(idx.remove)
+
+
 table(pheno$group)
 sum(is.na(pheno$group))
 
 stopifnot(pheno$sample_id == colnames(mat))
 
+mat <- mat[, !idx.remove];
+pheno <- pheno[!idx.remove, ];
 
 N <- ncol(mat);
 K <- length(levels(pheno$group));
@@ -50,6 +53,8 @@ K <- length(levels(pheno$group));
 #Calculate standard deviation within and between the groups
 
 genes <- rownames(mat)
+
+n.na <- apply(mat, 1, function(z) sum(is.na(z)));
 
 within_sds <- unlist(lapply(genes, function(gene) {
   get_within.sd(mat[gene, ], pheno$group)
@@ -60,9 +65,19 @@ between_sds <- unlist(lapply(genes, function(gene) {
 }));
 
 d.sd <- data.frame(gene = genes, within_sd = within_sds, between_sd = between_sds);
-qwrite(d.sd, "sds.rds")
+qwrite(d.sd, "sds.rds");
 
-housekeeping <- c("ACTB", "UBC", "GAPDH");
+# SSX9P has many NA and some groups have only NA
+# for groups with all missing value, the mean is NaN
+v.problematic <- mat["SSX9P", ];
+table(is.na(v.problematic))
+gmean.problematic <- tapply(v.problematic, pheno$group, mean, na.rm=TRUE);
+gmean.problematic
+overall_mean <- mean(gmean.problematic, na.rm=TRUE)
+#print(sum((group_mean-overall_mean)^2))
+between_sd.problematic <- sqrt(sum((gmean.problematic-overall_mean)^2, na.rm=TRUE) / (length(gmean.problematic)-1))
+
+housekeeping <- c("ACTB", "UBC", "GAPDH", "");  # TODO: Add more!
 dplyr::filter(d.sd, gene %in% housekeeping)
 
 d.sd.sub <- subset(d.sd, within_sd < 0.8)
@@ -77,4 +92,3 @@ ggplot(subset(d.sd.sub, within_sd < 0.25), aes(x=within_sd, y=between_sd, label=
   geom_label_repel() +
   geom_point() + coord_fixed() +
   geom_abline(yintercept=0, slope = sqrt((N - K) / (N - K - 2)))
-
