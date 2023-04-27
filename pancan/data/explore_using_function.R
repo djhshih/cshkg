3ed0156ef7165d62768e61cc4675df07b3b425d1
Yeri Han <hanyeri0223@gmail.com>
2023-04-27 18:36:56 +0800

    Update explore_using_functions: some plots

--- pancan/data/explore_using_function.R
+++ pancan/data/explore_using_function.R
@@ -1,18 +1,16 @@
-
 library(io)
 library(ggplot2)
 library(ggrepel)
 
-
+#import functions to calculate standard deviation
 source("sd_function.R")
 
 proj.info <- qread("../annot/sample-project-info_pancan.tsv")
-
 type.info <- qread("../annot/sample-type-code_tcga.tsv")
 
-
 x <- qread("expr_pancan.rds")
-mat <- x$data;
+mat0 <- x$data;
+mat <- mat0
 
 # Grouping the tissues
 idx <- match(substring(colnames(mat),1,16), proj.info$sample)
@@ -30,28 +28,41 @@ pheno <- data.frame(
 pheno$group <- factor(paste0(pheno$cancer_type, "-", pheno$sample_type));
 
 levels(pheno$group)
+no.groups.before <- length(levels(pheno$group)) # check the number of groups before filtering out the rare groups
 
+#filter out the rare groups
 rare.groups <- names(which(table(pheno$group) < 10));
-
 levels(pheno$group)[levels(pheno$group) %in% rare.groups] <- NA;
-
 idx.remove <- is.na(pheno$group);
-table(idx.remove)
+table(idx.remove)  # 87 samples that belong to rare groups were set to NA in the mat
 
+levels(pheno$group)
+no.groups.after <- length(levels(pheno$group)) # check the number of group after filtering out the rare groups
 
 table(pheno$group)
 sum(is.na(pheno$group))
 
+plot_1_df <- data.frame("filter" = factor(c("before", "after"), levels = c("before", "after")), "number_of_group" = c(no.groups.before, no.groups.after))
+
+
+ggplot(data = plot_1_df, aes(x = filter, y = number_of_group))+ 
+  ggtitle("Number of groups before and after filtering")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  xlab("filtering") + ylab("number of groups")+
+  geom_bar(stat = "identity") + theme_minimal()+ 
+  geom_text(aes(label= number_of_group), vjust=-1)
+
+
 stopifnot(pheno$sample_id == colnames(mat))
 
-mat <- mat[, !idx.remove];
+mat <- mat[, !idx.remove]; # 87 samples that belong to rare groups were set to NA in the mat
 pheno <- pheno[!idx.remove, ];
 
 N <- ncol(mat);
 K <- length(levels(pheno$group));
 
-#Calculate standard deviation within and between the groups
 
+#Calculate standard deviation within and between the groups
 genes <- rownames(mat)
 
 n.na <- apply(mat, 1, function(z) sum(is.na(z)));
@@ -71,6 +82,7 @@ d.sd <- data.frame(
 );
 
 qwrite(d.sd, "sds.rds");
+#d.sd <- qread("sds.rds") # load d.sd 
 
 # SSX9P has many NA and some groups have only NA
 # for groups with all missing value, the mean is NaN
@@ -78,38 +90,67 @@ v.problematic <- mat["SSX9P", ];
 table(is.na(v.problematic))
 gmean.problematic <- tapply(v.problematic, pheno$group, mean, na.rm=TRUE);
 gmean.problematic
-overall_mean <- mean(gmean.problematic, na.rm=TRUE)
+overall_mean <- mean(gmean.problematic, na.rm=TRUE) #this makes sure overall mean is not NaN
 #print(sum((group_mean-overall_mean)^2))
 between_sd.problematic <- sqrt(sum((gmean.problematic-overall_mean)^2, na.rm=TRUE) / (length(gmean.problematic)-1))
 
-housekeeping <- c("ACTB", "UBC", "GAPDH", "TBP", "RPS18", "G6PD", "HPRT1", "LDHA", "RPL19","RPL18","RPL11","RPL32", "PGK1", "PPIA", "RPS18", "ASNS","ATP2B4", "PEX19","RXRA");  # TODO: Add more!
-non_hk <- c("TP53","EGFR","AKT1")
-dplyr::filter(d.sd, gene %in% housekeeping)
-dplyr::filter(d.sd, gene %in% non_hk)
+
+# highlight common housekeeping genes and common genes
+housekeeping <- c("ACTB", "UBC", "GAPDH", "TBP", "RPS18", 
+                  "G6PD", "HPRT1", "LDHA", "RPLP1","RPL19",
+                  "RPL18", "RPL11","RPL32", "PGK1", "PPIA", 
+                  "RPS18","ASNS","ATP2B4", "PEX19","RXRA",
+                  "RPL13A","PPIA","SDHA","PPIA")  # TODO: Add more!
+common_gene <- c("TP53","EGFR","AKT1")
+hkg.sub <- dplyr::filter(d.sd, gene %in% housekeeping)
+dplyr::filter(d.sd, gene %in% common_gene)
+
+# plot all genes (btwn against within)
+pdf("plot2.pdf", width = 8, height = 8)
+ggplot(d.sd, aes(x=within_sd, y=between_sd))+
+  ggtitle("Between group SD against Within group SD")+
+  geom_point()+
+  theme(plot.title = element_text(hjust = 0.5))+
+  theme_linedraw()
+dev.off()
 
 d.sd.sub <- subset(d.sd, within_sd < 0.8)
+
+#remove genes with very low expression
+min_hkg_exp <- min(hkg.sub$mean)
+d.sd.sub <- subset(d.sd.sub, mean>=min_hkg_exp)
+
+#subset hkg and common genes
 hkg.sub <- subset(d.sd.sub, d.sd.sub$gene %in% housekeeping)
-non_hk.sub <- subset(d.sd, d.sd$gene %in% non_hk)
+common.sub <- subset(d.sd, d.sd$gene %in% common_gene)
 
 dim(d.sd.sub)
 
+
+#within_sd <0.8
+pdf("plot1.pdf", width = 8, height = 8)
 ggplot(d.sd.sub, aes(x=within_sd, y=between_sd, label=gene))+
   #geom_text_repel() +
   geom_point(alpha=0.1) + coord_fixed() +
   geom_point(hkg.sub, mapping=aes(color="red"))+ geom_label_repel(hkg.sub, mapping=aes(color="red"),label.padding = 0.1)+
-  geom_abline(yintercept=0, slope = sqrt((N - K) / (N - K - 2)))
+  geom_abline(yintercept=0, slope = sqrt((N - K) / (N - K - 2)))+
+  theme_minimal()
+dev.off()
 
-
+#within_sd < 0.25
 ggplot(subset(d.sd.sub, within_sd < 0.25), aes(x=within_sd, y=between_sd, label=gene))+
   geom_label_repel() +
   geom_point() + coord_fixed() +
   geom_point(hkg.sub, mapping=aes(color="red"))+ geom_label_repel(hkg.sub, mapping=aes(color="red"),label.padding = 0.1)+
-  geom_point(non_hk.sub, mapping=aes(color="blue"))+ geom_label(non_hk.sub, mapping=aes(color="blue"))+
+  geom_point(common.sub, mapping=aes(color="blue"))+ geom_label(common.sub, mapping=aes(color="blue"))+
   geom_abline(yintercept=0, slope = sqrt((N - K) / (N - K - 2)))
 
 
+#mean expression of genes
 ggplot(subset(d.sd.sub, within_sd < 0.25), aes(x=within_sd, y=mean, label=gene))+
   geom_label_repel() +
   geom_point() +
   geom_point(hkg.sub, mapping=aes(color="red"))+ geom_label_repel(hkg.sub, mapping=aes(color="red"),label.padding = 0.1)+
-  geom_point(non_hk.sub, mapping=aes(color="blue"))+ geom_label(non_hk.sub, mapping=aes(color="blue"))
+  geom_point(common.sub, mapping=aes(color="blue"))+ geom_label(common.sub, mapping=aes(color="blue"))
+
+
