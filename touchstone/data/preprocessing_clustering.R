254408989cb69439887be19beb22d29c6ca419c2
Yerim Rho <nyr85830@naver.com>
2024-01-09 22:19:34 +0800

    Hierarchial Clustering by genes

--- touchstone/data/preprocessing_clustering.R
+++ touchstone/data/preprocessing_clustering.R
@@ -2,6 +2,10 @@ library(cmapR)
 library(tidyr)
 library(dplyr)
 my_ds <- parse_gctx("GSE92742_Broad_LINCS_Level2_GEX_delta_n49216x978.gctx")
+# sample info
+inst_info <- read.delim("GSE92742_Broad_LINCS_inst_info.txt")
+# gene info
+gene_info_delta_landmark <- read.delim("GSE92742_Broad_LINCS_gene_info_delta_landmark.txt")
 
 # Group Clustering
 library(tidyverse)
@@ -10,41 +14,22 @@ library(factoextra)
 library(dendextend)
 # Touchstone Dataset Metadata
 m <- my_ds@mat
+genes_info <- my_ds@rdesc
 
-# group clustering
-library(tidyverse)
-library(cluster)
-library(factoextra)
-library(dendextend)
-# Perform hierarchical clustering on the transposed dataset (samples in rows, genes in columns)
-m_rev <- na.omit(t(m))
-#d <- dist(m_rev)
-#hc <- hclust(d) # not working
-
-# Subset matrix
-subset_size <- 100
-num_columns <- ncol(m_rev)
-# Ensure that the subset size is not larger than the number of columns
-if (subset_size > num_columns) {
-  stop("Subset size is larger than the number of columns in the dataset.")
-}
-
-# Error: Error: vector memory exhausted (limit reached?)
-# Sys.setenv("R_MAX_VSIZE" = 32000000000) # not working
-
-subset_m <- m_rev[, sample(num_columns, size = subset_size, replace = FALSE)]
-saveRDS(subset_m, file = "subset_m.rds")
-hc <- hclust(dist(as.matrix(subset_m)))
-
+# Group Clustering
+# Hierarchical clustering (samples in columns, genes in rows)
+d <- dist(m)
+hc <- hclust(d)
 # Plot the dendrogram
 dend <- as.dendrogram(hc)
-dend <- color_branches(dend, k = 4)
+dend <- color_branches(dend, k = 10)
 # Visualize the dendrogram
 plot(dend, main = "Hierarchical Clustering Dendrogram", xlab = "Samples", sub = "")
 
 # Cut the tree to get clusters
-clusters <- cutree(hc, h = 50)  # Adjust the height (h) based on the dendrogram
-dim(df_group)
+clusters <- cutree(hc, h = 400000)  # Adjust the height (h) based on the dendrogram
+dim(genes_info)
 length(clusters)
 # Combine the cluster information with your sample grouping data
-result_df <- cbind(df_group, Cluster = clusters)
+result_df <- cbind(genes_info, Cluster = clusters)
+unique(result_df$Cluster) # total 44 groups
