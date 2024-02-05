fbe7c523f8fa87c468dedda7996d0f35296c2eb8
Yerim Rho <nyr85830@naver.com>
2024-02-05 22:21:57 +0800

    Updated runPCA

--- touchstone/data/runPCA.R
+++ touchstone/data/runPCA.R
@@ -2,12 +2,20 @@
 library(corrr)
 library(ggcorrplot)
 library(FactoMineR)
+print("loaded libraries")
 # Touchstone Dataset Metadata
 load("m.rds")
 load("m_genes.rds")
 load("touchstone_sample.rds")
 load("samples.rds")
+print("check dataset loading")
 
 # Current samples are divided depends on cell id and perturbagen (total 2380 groups)
-corr_matrix <- cor(m_genes)
-ggcorrplot(corr_matrix)
+m_sub <- m_genes[,1:40000]
+transposed_m_genes <- t(m_genes)
+pca_result <- prcomp(transposed_m_genes, scale. = TRUE)
+summary(pca_result)
+
+p_components <- pca_result$x
+pca_df <- as.data.frame(p_components)
+write.table(pca_df, file="pca_results.txt", sep="\t", row.names=FALSE)
