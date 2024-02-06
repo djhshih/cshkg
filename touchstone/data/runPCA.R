b90cddabaed55ef7bb6378c7489414d86e466920
Yerim Rho <nyr85830@naver.com>
2024-02-06 15:43:14 +0800

    Divide matrixes depends on 3 different cell types to run PCA

--- touchstone/data/runPCA.R
+++ touchstone/data/runPCA.R
@@ -11,9 +11,18 @@ load("samples.rds")
 print("check dataset loading")
 
 # Current samples are divided depends on cell id and perturbagen (total 2380 groups)
-m_sub <- m_genes[,1:40000]
-transposed_m_genes <- t(m_genes)
-pca_result <- prcomp(transposed_m_genes, scale. = TRUE)
+# Divide matrix by cell types "VCAP", "MCF7", "PC3" (total 3 matrixes)
+# s_VCAP <- touchstone_sample[which(touchstone_sample$cell_id == "VCAP"),]
+# m_VCAP <- m_genes[, intersect(colnames(m_genes), s_VCAP$inst_id)]
+# dim(m_VCAP) # 978 genes x 5078 samples
+# s_MCF7 <- touchstone_sample[which(touchstone_sample$cell_id == "MCF7"),]
+# m_MCF7 <- m_genes[, intersect(colnames(m_genes), s_MCF7$inst_id)]
+# dim(m_MCF7) # 978 genes x 21776 samples
+# s_PC3 <- touchstone_sample[which(touchstone_sample$cell_id == "PC3"),]
+# m_PC3 <- m_genes[, intersect(colnames(m_genes), s_PC3$inst_id)]
+# dim(m_PC3) # 978 genes x 22353 samples
+
+pca_result <- prcomp(t(m_genes), scale. = TRUE)
 summary(pca_result)
 
 p_components <- pca_result$x
